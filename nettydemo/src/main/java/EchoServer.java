94ffce9b1f3309eafd62d634457a965c2a629621
deeplinux <1114392851@qq.com>
2018-10-22 17:45:39 +0800

    增加netty服务端代码

--- nettydemo/src/main/java/EchoServer.java
+++ nettydemo/src/main/java/EchoServer.java
@@ -1,2 +1,27 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class EchoServer {
+    public static void main(String[] args) throws InterruptedException {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(group)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(8888)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(serverHandler);
+                    }
+                });
+        ChannelFuture f = b.bind().sync();
+        f.channel().closeFuture().sync();
+    }
 }
